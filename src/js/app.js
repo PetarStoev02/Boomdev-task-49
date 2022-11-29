10c7c3b2e638b5cb7763d66ae719f0f0da3511ba
Петър Стоев <petarstoev0505@gmail.com>
2022-11-29 23:36:25 +0200

    v6

--- src/js/app.js
+++ src/js/app.js
@@ -6,45 +6,42 @@ window.addEventListener("DOMContentLoaded", () => {
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
-    console.log(cardName.name)
-    console.log(cardNumber.name)
-    console.log(cardCVC.name)
-    console.log(cardExpiry.name)
   });
 
  
-  var form = document.createElement("form");
-  form.setAttribute("method", "post");
-  form.setAttribute("action", "");
+  // var form = document.createElement("form");
+  // form.setAttribute("method", "post");
+  // form.setAttribute("action", "");
   
 
-  var cardName = document.createElement("input");
-  cardName.type = "text";
-  cardName.className = "ccname"; 
-  form.appendChild(cardName); 
+  // var cardName = document.createElement("input");
+  // cardName.type = "text";
+  // cardName.className = "ccname"; 
+  // form.appendChild(cardName); 
 
-  var cardNumber = document.createElement("input");
-  cardNumber.type = "text";
-  cardNumber.className = "cardnumber"; 
-  form.appendChild(cardNumber); 
+  // var cardNumber = document.createElement("input");
+  // cardNumber.type = "text";
+  // cardNumber.className = "cardnumber"; 
+  // form.appendChild(cardNumber); 
 
-  var cardCVC = document.createElement("input");
-  cardCVC.type = "text";
-  cardCVC.className = "cardnumber"; 
-  form.appendChild(cardCVC); 
+  // var cardCVC = document.createElement("input");
+  // cardCVC.type = "text";
+  // cardCVC.className = "cardnumber"; 
+  // form.appendChild(cardCVC); 
 
-  var cardExpiry = document.createElement("input");
-  cardExpiry.type = "text";
-  cardExpiry.className = "cc-exp"; 
-  form.appendChild(cardExpiry); 
+  // var cardExpiry = document.createElement("input");
+  // cardExpiry.type = "text";
+  // cardExpiry.className = "cc-exp"; 
+  // form.appendChild(cardExpiry); 
 
-  document.getElementsByTagName("body")[0]
-               .appendChild(form);
+  // document.getElementsByTagName("body")[0]
+  //              .appendChild(form);
 });
 
-let cardName = document.querySelector('input[name="ccname"]');
-let cardNumber = document.querySelector('input[name="cardnumber"]');
-let cardCVC = document.querySelector('input[name="cvc"]');
-let cardExpiry = document.querySelector('input[name="cc-exp"]');
+// Check if input.name exists
+// let cardName = document.querySelector('input[name="ccname"]');
+// let cardNumber = document.querySelector('input[name="cardnumber"]');
+// let cardCVC = document.querySelector('input[name="cvc"]');
+// let cardExpiry = document.querySelector('input[name="cc-exp"]');
 
 
